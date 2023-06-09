8370f8784eeac3008fd942b52c5b51ad3b90f7a5
lanrad09 <lanrad09@gmail.com>
2023-06-09 09:01:55 +0200

    create a backend folder , write a server code , install expresss, body-parser and express packages; set up a nodejs backend application.install nodemon pac to automate the restartig of nodejs server whenever a change occure in the node application

---
+++ backend/app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.use((req, res, next)=> {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+    next();
+});
+
+app.get("/api/products", (req, res, next)=> {
+    const products = [
+        {
+            id: "faderese1",
+            name: "shoe1",
+            gtin: "1234321234567",
+            brandName: "addidas",
+            price: "23",
+        },
+        {
+            id: "aaderese1",
+            name: "cap",
+            gtin: "1234321234567",
+            brandName: "Nike",
+            price: "33",
+        },
+        {
+            id: "aaderese1",
+            name: "belt",
+            gtin: "1234321234567",
+            brandName: "Panda",
+            price: "334",
+        },
+
+    ];
+    res.status(200).json({
+        message: "Products fectched successfully!",
+        products : products
+    });
+})
+
+
+module.exports = app;
